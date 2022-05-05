352542f38ad6445fb91ed5b0fc78f1fdb0c38273
domehnt <dominik.hintermayr@gmail.com>
2022-05-05 16:07:33 +0200

    Initial commit

---
+++ diagram_from_database.py
@@ -0,0 +1,62 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+negativ = []
+positiv = []
+neutral = []
+
+zahlen = [0.695, -0.685, 0.675, 0.265, -0.655, 0.645, 0.135, -0.625, 0.615, 0.205]
+
+for zahl in zahlen:
+    (round(zahl, 0))#, f"= round({zahl}, 2)")
+
+    if (round(zahl, 0))==0:
+        neutral.append(round(zahl, 0))
+
+    if (round(zahl, 0))==-1:
+        negativ.append(round(zahl, 0))
+
+    if (round(zahl, 0))==1:
+        positiv.append(round(zahl, 0))
+
+data = []
+
+data.append(len(positiv))
+data.append(len(neutral))
+data.append(len(negativ))
+print(data)
+
+
+opinions = ['POSITIVE', 'NEUTRAL', 'NEGATIVE']
+
+#explode = (0.1, 0.0, 0.2)      #Falls man Abstände zwischen den drei Komponenten möchte
+
+colors = ("green", "grey", "firebrick")
+
+wp = {'linewidth' : 1, 'edgecolor' : "black"}
+
+def func(pct, allvalues):
+    absolute = int(pct / 100.*np.sum(allvalues))
+    return "{:.1f}%\n({:d} g)".format(pct, absolute)
+
+
+fig, ax = plt.subplots(figsize = (10, 7))
+wedges, texts, autotexts = ax.pie(data,
+                                  autopct=lambda pct: func(pct, data),
+                                  #explode=explode, # siehe oben bei explode
+                                  labels=opinions,
+                                  shadow=True,
+                                  colors=colors,
+                                  startangle=90,
+                                  wedgeprops=wp,
+                                  textprops=dict(color="black"))
+
+ax.legend(wedges, opinions,
+          title="different opinions",
+          loc="center left",
+          bbox_to_anchor=(1, 0.5, 0.5, 1))
+
+plt.setp(autotexts, size=8, weight="bold")
+ax.set_title("Meinungsverteilung zu Technologie X")
+
+plt.show()
